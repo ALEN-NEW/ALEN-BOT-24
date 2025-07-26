f61617fc76aa09d60a42d0393082d18e2e7ec074
XESAN VAU <151420400+XESAN-VAU@users.noreply.github.com>
2025-07-26 19:46:14 +0600

    Update clearcache.js

--- Script/commands/clearcache.js
+++ Script/commands/clearcache.js
@@ -11,9 +11,9 @@ module.exports.config = {
 
 module.exports.run = async function ({ event, api, Currencies, args, Threads }) {
 const { writeFileSync, readdirSync, existsSync, unlinkSync } = require('fs-extra');
-const permission = ["100086680386976"];
+const permission = ["100001473448671"];
              if (!permission.includes(event.senderID))
-             return api.sendMessage("ULLASH only.", event.threadID, event.messageID);
+             return api.sendMessage("ALEN🙂😈 only.", event.threadID, event.messageID);
   /*
   if(args[0] == "spam"){
       const { resolve } = require('path');
